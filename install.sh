ea5712f3296eeb9b359689a4c30e2885de59e424
Eric Stewart <estewart@sans.org>
2016-10-14 20:24:48 -0400

    Create ~/bin on installation.

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+# Copy .zshrc to ~
+echo "Installing .zshrc"
 cp .zshrc $HOME/
+
+# Create ~/bin directory if it doesn't exist.
+if [ ! -d "$HOME/bin" ]; then
+  echo "Creating $HOME/bin."
+  mkdir -p "$HOME/bin"
+fi
